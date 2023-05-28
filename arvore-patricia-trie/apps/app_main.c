0922f8c52b8acc5673c86a6567aaa9bfbcfab388
Mateus Pepice <mateuspepice@hotmail.com>
2023-05-28 17:21:06 -0300

    Arquivos adicionados e alterados

--- arvore-patricia-trie/apps/app_main.c
+++ arvore-patricia-trie/apps/app_main.c
@@ -2,12 +2,65 @@
 #include <stdio.h>
 #include <stdlib.h> 
 
+void adicionarChave(){
+   printf("VALOR QUE DESEJA INSERIR: \n");
+}
+
+void removerChave(){
+
+}
+
+void imprimirArvore(){
+
+}
+
+void buscarValor(){
+
+}
 
 int main() { 
-    
-   printf("\n--- Árvore Digital de Busca, Tries e Patricia Tries ---\n\n");
+   int escolha; 
    
- 
+   printf("\n--- Arvore Digital de Busca, Tries e Patricia Tries ---\n\n");
+
+   do
+   {
+      printf("ESCOLHA UMA FUNCAO PARA REALIZAR:\n");
+      printf("1 - ADICIONAR CHAVE\n");
+      printf("2 - REMOVER CHAVE\n");
+      printf("3 - IMPRIMIR ARVORE\n");
+      printf("4 - BUSCAR VALOR\n");
+      printf("5 - DESTRUIR ARVORE\n");
+      printf("0 - SAIR\n");
+
+      scanf("%i", &escolha);
+
+      switch (escolha)
+      {
+      case 1:
+         
+         break;
+      case 2:
+         /* code */
+         break;
+      case 3:
+         /* code */
+         break;
+      case 4:
+         /* code */
+         break;
+      case 5:
+         /* code */
+         break;
+      case 0:
+         printf("SAINDO DO PROGRAMA...");
+         break;
+      default:
+      printf("OPCAO INVALIDA!!");
+         break;
+      }
+   } while (escolha !=0);
+
    return 0;
 
 }
